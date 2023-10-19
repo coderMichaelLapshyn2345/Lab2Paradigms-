4d0e5a7671f9674bd3e2f129b11f2771762fb7fc
Mykhailo Lapshyn <lapshynmisha@gmail.com>
2023-10-19 09:17:28 +0200

    add basic class

--- Lab2 Paradigms/main.cpp
+++ Lab2 Paradigms/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <set>
 using namespace std;
+class FunctionalObjects {
+private:
+    set<int> something;
+public:
+    set<int> GetSet(){
+        return this->something;
+    }
+};
 int main(){
     return 0;
 }
